de30ac46096c6218c7d355a499d9dc4bd82a70b0
Jason Conley <70329226+jasonbconley@users.noreply.github.com>
2022-12-14 19:20:49 -0500

    Adding java http server

--- myhttphandler/MainPageHandler.java
+++ myhttphandler/MainPageHandler.java
@@ -10,9 +10,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-
 import java.util.ArrayList;
-import java.util.Array;
+import java.util.Base64;
 
 public class MainPageHandler implements HttpHandler {
     @Override    
@@ -24,35 +23,38 @@ public class MainPageHandler implements HttpHandler {
         String line;
         ArrayList<Byte> byteLine = new ArrayList<>();
         ArrayList<Byte> response = new ArrayList<>();
-        String responseHtml = null;
+        byte[] responseHtml = null;
 
         // This code needs to be changed to read in images as well. 
         // May be better to use bytes object from get go, and if the line has an image in it, then the according file needs to be found and opened to be read.
-
         try {
-            File pageFile = new File("../index.html");
-            BufferedReader bufferedReader = 
-                    new BufferedReader(
-                    new InputStreamReader(
-                    new FileInputStream(pageFile)));
+            File pageFile = new File("index.html");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(pageFile)));
             while ((line = bufferedReader.readLine()) != null) {
                 byteLine = translateLine(line);
                 response.addAll(byteLine);
             }
             bufferedReader.close();
-            responseHtml = response.toArray();
+            responseHtml = new byte[response.size()];
+            int i = 0;
+            for (Byte b : response) {
+                responseHtml[i++] = b.byteValue();
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Error finding file");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error reading file");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Error in class");
         }
 
         try {
-            httpEx.sendResponseHeaders(200, responseHtml.length());
+            httpEx.sendResponseHeaders(200, responseHtml.length);
             OutputStream os = httpEx.getResponseBody();
-            os.write(responseHtml.getBytes());
+            os.write(responseHtml);
             os.flush();
             os.close();
         } catch (IOException e) {
@@ -62,32 +64,71 @@ public class MainPageHandler implements HttpHandler {
         }
     }
     
+    // Convert line in html to bytes, encoding images in base64 prior to byteifying 
+    private ArrayList<Byte> translateLine(String line) throws ClassNotFoundException, IOException {
+        ArrayList<Byte> byteArray = new ArrayList<>();
+        int srcIndex = line.indexOf("src=");
+        byte[] byteLine;
+
+        // This means there is an image to encode to base64, need to translate string before and after to bytes
+        if (srcIndex != -1) {
+
+            int firstQuote = srcIndex + 5;
+            String afterQuote = line.substring(firstQuote);
+            int lastQuote = afterQuote.indexOf("\"");
+
+            // Do first part of String
+            String firstPart = line.substring(0, firstQuote);
+            byteLine = firstPart.getBytes();
+            for (final byte val : byteLine) {
+                byteArray.add(val);
+            }
+
+            // Do image file
+            String imageFile = line.substring(firstQuote, firstQuote + lastQuote);
+            String imageText = img2Text(imageFile);
+            byteLine = imageText.getBytes();
+            for (final byte val : byteLine) {
+                byteArray.add(val);
+            }
+
+            // Do second part of string
+            String secondPart = line.substring(firstQuote + lastQuote);
+            byteLine = secondPart.getBytes();
+            for (final byte val : byteLine) {
+                byteArray.add(val);
+            }
+
+        } else {
+            byte[] wholeLineBytes = StringToByte(line);
+            for (final byte val : wholeLineBytes) {
+                byteArray.add(val);
+            }
+        }
+        return byteArray;  
+    }
+
+    // Convert image to base64 text
     public String img2Text(String path){
         String base64="";
         // Grab file type
-        String fileType = path.substring(word.length() - 3);
+        String fileType = path.substring(path.lastIndexOf('.') + 1);
         try{
-            InputStream iSteamReader = new FileInputStream("../" + path);
-            byte[] imageBytes = IOUtils.toByteArray(iSteamReader);
+            File file = new File(path);
+            byte[] imageBytes = new byte[(int) file.length()];
+            FileInputStream fis = null;
+            fis = new FileInputStream(file);
+            fis.read(imageBytes);
+            fis.close();
             base64 = Base64.getEncoder().encodeToString(imageBytes);
-            System.out.println(base64);
         }catch(Exception e){
-            e.printStackTrace();
+            e.printStackTrace(); 
         }
         return "data:image/" + fileType + ";base64," + base64;
     }
-    
-    private ArrayList<Byte> translateLine(line) {
-        ArrayList<Byte> byteArray = new ArrayList<>();
-        int srcIndex = line.indexOf("src=");
-        // This means there is an image to encode to base64, might need to make size -1
-        if (srcIndex != line.size()) {
-            int firstQuote = // find first quote 
-            int lastQuote = // find last quote
-            String imageFile = line.substring(firstQuote+1, lastQuote-1);
-            ArrayList<Byte> imageBytes = img2Text(imageFile).getBytes();
-            byteArray.addAll(imageBytes);
-        }
-        
+
+    private byte[] StringToByte(String line) {
+        byte[] lineBytes = line.getBytes();
+        return lineBytes;
     }
 }
